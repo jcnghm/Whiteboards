523df6d20a0c4066f23cf17e42c4c3ce33fc9e99
Joshua Cunningham <86850223+jcnghm@users.noreply.github.com>
2021-11-04 14:50:47 -0700

    updates

--- student_pick.py
+++ student_pick.py
@@ -2,18 +2,18 @@
 
 # TALLY:
 
-# William Blumenthal 
-# Asher Boucher 
-# John Brandon 
-# Brandon Weathersby
-# Quamel Buckmire
+# William B
+# Asher B
+# John B
+# Brandon W
+# Quamel B
 # Alex E
-# Allan Einhorn
-# Shouwang Huang
-# Samantha Manning
-# Iwona Morawska
-# Matias Nazabal
-# Christopher Schellenger
+# Allan E
+# Shouwang H
+# Samantha M
+# Iwona M
+# Matias N
+# Christopher S
 
 import random
 students = ["William", "Asher", "John", "Brandon", "Quamel", "Alex", "Allan", "Shouwang", "Samantha", "Iwona", "Matias", "Christopher"]

1092162a0ab71d171304ceb4d5f04696a963454f
Joshua Cunningham <86850223+jcnghm@users.noreply.github.com>
2021-11-15 13:04:42 -0800

    formatting updates

--- medium/move_zeros.py
+++ medium/move_zeros.py
@@ -1,5 +1,6 @@
 # Move Zeros
-# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Given an array nums, write a function to move all 0's to the end of it 
+# while maintaining the relative order of the non-zero elements.
 # Example:
 # Input: [0,1,0,3,12]
 # Output: [1,3,12,0,0]
@@ -8,13 +9,13 @@
 
 
 def moveZeros(array):
-   counter = 0
-   while 0 in array:
+    counter = 0
+    while 0 in array:
        array.remove(0)
        counter += 1
-   for i in range(counter):
+    for i in range(counter):
        array.append(0)
       
-   return(array)
+    return(array)
 
 print(moveZeros([0,1,0,3,12]))

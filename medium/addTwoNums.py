0b7cdae4eeedcb72a3b0fa7118811de8031bf5a7
Joshua Cunningham <86850223+jcnghm@users.noreply.github.com>
2021-12-02 17:49:03 -0800

    add

--- medium/addTwoNums.py
+++ medium/addTwoNums.py
@@ -1,22 +1,24 @@
-# def addTwoNumbers(l1, l2):
-#     one = l1[::-1]
-#     two = l2[::-1]
-#     lst1 = []
-#     lst2 = []
-#     for i in one:
-#         lst1.append(str(i))
-#     for i in two:
-#         lst2.append(str(i))
-#     lst1 = ''.join(lst1)
-#     lst2 = ''.join(lst2)
-#     lst1 = int(lst1)
-#     lst2 = int(lst2)
-#     sum = lst1 + lst2
-#     sum = str(sum)
-#     solution = sum[::-1]
-#     return int(solution)
+# Leetcode problem
 
-# addTwoNumbers([2,4,3],[5,6,4])
+def addTwoNumbers(l1, l2):
+    one = l1[::-1]
+    two = l2[::-1]
+    lst1 = []
+    lst2 = []
+    for i in one:
+        lst1.append(str(i))
+    for i in two:
+        lst2.append(str(i))
+    lst1 = ''.join(lst1)
+    lst2 = ''.join(lst2)
+    lst1 = int(lst1)
+    lst2 = int(lst2)
+    sum = lst1 + lst2
+    sum = str(sum)
+    solution = sum[::-1]
+    return int(solution)
+
+addTwoNumbers([2,4,3],[5,6,4])
 
 
 
